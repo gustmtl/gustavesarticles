4f799909c66435485f705bea76fdcafb11e8f59e
Gustave Laurant <tutal@protonmail.com>
2020-03-27 17:34:38 +0100

    content of 3/4 tabs done, reste bitcoin

--- server.py
+++ server.py
@@ -8,6 +8,18 @@ heroku = Heroku(app)
 def show_home():
     return render_template('home.html')
 
+@app.route("/articles", methods= ['GET'])
+def show_articles():
+    return render_template('articles.html')
+
+@app.route("/bitcoin", methods= ['GET'])
+def show_bitcoin():
+    return render_template('bitcoin.html')
+
+@app.route("/videos", methods= ['GET'])
+def show_videos():
+    return render_template('videos.html')
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
